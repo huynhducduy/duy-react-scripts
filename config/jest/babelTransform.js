a1f6cb107e8a80d2ceb35e301a99a048b28af0bf
Huynh Duc Duy <duy.huynh@linecorp.com>
2021-04-20 15:41:21 +0700

    update babel-jest config

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -3,28 +3,7 @@
 
 const babelJest = require('babel-jest')
 
-const hasJsxRuntime = (() => {
-  if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
-    return false
-  }
-
-  try {
-    require.resolve('react/jsx-runtime')
-    return true
-  } catch (e) {
-    return false
-  }
-})()
-
 module.exports = babelJest.createTransformer({
-  presets: [
-    [
-      require.resolve('@babel/preset-react'),
-      {
-        runtime: hasJsxRuntime ? 'automatic' : 'classic',
-      },
-    ],
-  ],
-  babelrc: false,
-  configFile: false,
+  babelrc: true,
+  configFile: true,
 })
