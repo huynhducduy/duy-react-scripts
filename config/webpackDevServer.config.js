c2c294553559777b13b6a5db27a9f92bbf18f418
Huynh Duc Duy <duy.huynh@linecorp.com>
2021-08-23 12:03:39 +0700

    v1.0.22

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -97,23 +97,23 @@ module.exports = function (proxy, allowedHost) {
       // Keep `evalSourceMapMiddleware`
       // middlewares before `redirectServedPath` otherwise will not have any effect
       // This lets us fetch source contents from webpack for the error overlay
-      devServer.use(evalSourceMapMiddleware(devServer))
+      devServer.app.use(evalSourceMapMiddleware(devServer));
 
       if (fs.existsSync(paths.proxySetup)) {
         // This registers user provided middleware for proxy reasons
-        require(paths.proxySetup)(devServer.app)
+        require(paths.proxySetup)(devServer.app);
       }
     },
     onAfterSetupMiddleware(devServer) {
       // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
-      devServer.use(redirectServedPath(paths.publicUrlOrPath))
+      devServer.app.use(redirectServedPath(paths.publicUrlOrPath));
 
       // This service worker file is effectively a 'no-op' that will reset any
       // previous service worker registered for the same host:port combination.
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      devServer.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath))
+      devServer.app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
     },
-  }
-}
+  };
+};
